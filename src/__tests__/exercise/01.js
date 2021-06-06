70b07301f7495828e272d54fcfd76ab24cfcf27d
P S L Yaswanth <yashu.vit@gmail.com>
2021-06-06 23:43:46 +0530

    First exercise

---
+++ src/__tests__/exercise/01.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {screen, render} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import App from '../../exercise/01'
+
+test('1st exercise', () => {
+  render(<App />)
+
+  const input = screen.getByRole('textbox', /name/i)
+  userEvent.type(input, 'Yaswanth')
+
+  expect(screen.getByText(/hello.*yaswanth/i)).toBeInTheDocument()
+})
