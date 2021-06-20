16eb4758ad66968f98754dd438a228c8a563c1a8
P S L Yaswanth <yashu.vit@gmail.com>
2021-06-20 12:39:57 +0530

    End of exercise 5

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -7,7 +7,20 @@ import VanillaTilt from 'vanilla-tilt'
 
 function Tilt({children}) {
   // 🐨 create a ref here with React.useRef()
+  const tiltRef = React.useRef()
 
+  React.useEffect(() => {
+    const tiltNode = tiltRef.current
+
+    VanillaTilt.init(tiltNode, {
+      max: 25,
+      speed: 400,
+      glare: true,
+      'max-glare': 0.5,
+    })
+
+    return () => tiltNode.VanillaTilt.destroy()
+  }, [])
   // 🐨 add a `React.useEffect` callback here and use VanillaTilt to make your
   // div look fancy.
   // 💰 like this:
@@ -29,7 +42,7 @@ function Tilt({children}) {
 
   // 🐨 add the `ref` prop to the `tilt-root` div here:
   return (
-    <div className="tilt-root">
+    <div ref={tiltRef} className="tilt-root">
       <div className="tilt-child">{children}</div>
     </div>
   )
